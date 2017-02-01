728fb243e9a93cba80059e2e154f356743f68415
Graham <mouckatron@gmail.com>
2017-02-01 12:44:16 +0000

    updated countoutput to work with pip installed scripts

--- pyprogress/countoutput.py
+++ pyprogress/countoutput.py
@@ -3,33 +3,38 @@
 
 import sys
 
-if '--total' in sys.argv:
-    total = sys.argv[sys.argv.index('--total')+1]
-else:
-    total = None
 
-if '--pb' in sys.argv and total is not None:
-    from pyprogress import ThreadedProgressBar
-    c = ThreadedProgressBar(int(total), timecount=True, completionprediction=True, colored=True)
-else:
-    from pyprogress import Counter
-    c = Counter(total=total)
-c.start()
+def main():
+    if '--total' in sys.argv:
+        total = sys.argv[sys.argv.index('--total')+1]
+    else:
+        total = None
 
-while True:
-    try:
-        line = sys.stdin.readline()
-        if len(line) > 0:
-            c.inc()
-        else:
+    if '--pb' in sys.argv and total is not None:
+        from pyprogress import ThreadedProgressBar
+        c = ThreadedProgressBar(int(total), timecount=True, completionprediction=True, colored=True)
+    else:
+        from pyprogress import Counter
+        c = Counter(total=total)
+    c.start()
+
+    while True:
+        try:
+            line = sys.stdin.readline()
+            if len(line) > 0:
+                c.inc()
+            else:
+                break
+        except KeyboardInterrupt:
             break
-    except KeyboardInterrupt:
-        break
 
-if '--pb' in sys.argv and total is not None:
-    c.finish()
-else:
-    c.stop()
-c.join()
+    if '--pb' in sys.argv and total is not None:
+        c.finish()
+    else:
+        c.stop()
+    c.join()
 
-print ""
+    print ""
+
+if __name__ == '__main__':
+    main()
